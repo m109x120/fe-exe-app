813e081574b4f5749aaadae03c4a2ad5821857e6
yangmengxia <yangmengxia@weimiao.cn>
2022-04-24 20:32:58 +0800

    feat: 简单路由配置

--- src/main.js
+++ src/main.js
@@ -1,7 +1,12 @@
 import { createApp } from 'vue'
 import ElementPlus from 'element-plus'
+import router from './router'
 import 'element-plus/dist/index.css'
 import App from './App.vue'
 console.log('环境变量： ', import.meta.env)
+
 const app  = createApp(App)
-app.use(ElementPlus).mount('#app')
+
+app.use(ElementPlus)
+   .use(router)
+   .mount('#app')
---
+++ src/views/home/index.vue
@@ -0,0 +1,5 @@
+<template>
+  <h1>hello</h1>
+  <div>欢迎来到v3的世界</div>
+  <ElButton type="success">haha</ElButton>
+</template>
