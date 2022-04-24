d312c562c153698aada73c3a06c2ff015a1c34d9
yangmengxia <yangmengxia@weimiao.cn>
2022-04-24 19:56:10 +0800

    feat: 环境变量取值

--- src/main.js
+++ src/main.js
@@ -2,6 +2,6 @@ import { createApp } from 'vue'
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 import App from './App.vue'
-
+console.log('环境变量： ', import.meta.env)
 const app  = createApp(App)
 app.use(ElementPlus).mount('#app')
